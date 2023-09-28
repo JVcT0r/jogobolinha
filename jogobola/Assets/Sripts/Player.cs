b19220577b2ddeb7b91e617f840d7fce7b0465fe
JvctrM <39511255+JvctrM@users.noreply.github.com>
2023-09-28 08:04:32 -0300

    Camera e pulo

--- jogobola/Assets/Sripts/Player.cs
+++ jogobola/Assets/Sripts/Player.cs
@@ -8,8 +8,10 @@ public class Player : MonoBehaviour
 
 {
 
-    public int velocidade = 10;
+    public int forcaPulo = 9;
+    public int velocidade = 8;
     private Rigidbody rb;
+    public bool noChao; 
 
 
     // Start is called before the first frame update
@@ -19,20 +21,42 @@ public class Player : MonoBehaviour
         TryGetComponent(out rb);
     }
 
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(!noChao && collision.gameObject.tag == "Chao")
+        {
+            noChao = true;
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+        
 
-	Vector3 direcao = new Vector3(h, 0, v);
-    rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
+	    Vector3 direcao = new Vector3(h, 0, v);
+        rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
+        
+        if(Input.GetKeyDown(KeyCode.Space) && noChao)
+        {
+            rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+            noChao = false;
+        }
+    
 
-    if(transform.position.y <= -10)
-    {
-        //Jogador caiu
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
+
+
+
+
+
+        if(transform.position.y <= -10)
+        {
+            //Jogador caiu
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
 
     }
 }
